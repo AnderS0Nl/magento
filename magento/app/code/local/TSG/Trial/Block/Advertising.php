50877e2bc7e6821ee8eca123482304d66314ab3a
AnderS0Nl <TsubynaRuslan@gmail.com>
2018-12-31 14:41:39 +0200

    Updated for task 3

--- magento/app/code/local/TSG/Trial/Block/Advertising.php
+++ magento/app/code/local/TSG/Trial/Block/Advertising.php
@@ -3,8 +3,8 @@ class TSG_Trial_Block_Advertising extends Mage_Core_Block_Template
 {
     public function isAdvertising()
     {
-        $current_product = Mage::registry('current_product');
-        $sku = $current_product->getSku();
+        $currentProduct = Mage::registry('current_product');
+        $sku = $currentProduct->getSku();
         if ($sku == 'advertising') {
             return true;
         } else {
