25a62dbea4e458d2b63f3dc3e8b586bbc86e196f
AnderS0Nl <TsubynaRuslan@gmail.com>
2019-01-03 12:06:17 +0200

    Added updated files

--- app/code/local/TSG/Trial/Block/Advertising.php
+++ app/code/local/TSG/Trial/Block/Advertising.php
@@ -1,5 +1,14 @@
 <?php
 class TSG_Trial_Block_Advertising extends Mage_Core_Block_Template
 {
-
+    public function isAdvertising()
+    {
+        $currentProduct = Mage::registry('current_product');
+        $sku = $currentProduct->getSku();
+        if ($sku == 'advertising') {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
