fafaad54036ef57e63d922fdb9e2a34efa7f13b5
AnderS0Nl <TsubynaRuslan@gmail.com>
2019-01-09 10:13:18 +0200

    IndexController is updated (correction of mistakes)

--- app/code/local/TSG/Trial/controllers/IndexController.php
+++ app/code/local/TSG/Trial/controllers/IndexController.php
@@ -1,14 +1,9 @@
 <?php
 
-class TSG_Trial_IndexController extends Mage_Core_Controller_Front_Action
+class Tsg_Trial_IndexController extends Mage_Core_Controller_Front_Action
 {
-
     public function indexAction()
     {
         echo 'Hello Magento';
     }
-
 }
-
-
-
