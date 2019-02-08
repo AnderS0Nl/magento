e51536acafc738cd039c901c9744a07a4168b043
AnderS0N <TsubynaRuslan@gmail.com>
2019-02-08 15:43:32 +0200

    Correction of mistakes

--- app/code/local/TSG/Trial/Block/Advertising.php
+++ app/code/local/TSG/Trial/Block/Advertising.php
@@ -2,12 +2,5 @@
 
 class Tsg_Trial_Block_Advertising extends Mage_Core_Block_Template
 {
-    const SKU = 'advertising';
-
-    public function isAdvertising()
-    {
-        $currentProduct = Mage::registry('current_product');
-        $sku = $currentProduct->getSku();
-        return $sku === self::SKU;
-    }
+    
 }
